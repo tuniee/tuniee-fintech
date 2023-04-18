91d5e00a7021170e35fdb20d70a7130a48c48f9c
Elikwu Chekwube Anthony <tunieehandsome@gmail.com>
2023-04-18 21:04:04 +0100

    some changes and additions

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-function validateForm() {
-	var firstName = document.forms["myForm"]["firstName"].value;
-	var lastName = document.forms["myForm"]["lastName"].value;
-	var email = document.forms["myForm"]["email"].value;
-	var phone = document.forms["myForm"]["phone"].value;
+// Login Form validation
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
 
-	if (firstName == "" || lastName == "" || email == "" || phone == "") {
-		alert("Please fill in all fields");
-		return false;
-	}
-}
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
+
+  if (username === "user" && password === "webdev") {
+    alert("You have successfully logged in.");
+    window.location.href = "index.html";
+  } else {
+    loginErrorMsg.style.opacity = 1;
+  }
+});
+
+// Signup Form validation
+const signupForm = document.getElementById("signup-form");
+const signupButton = document.getElementById("signup-form-submit");
+const signupErrorMsg = document.getElementById("signup-error-msg");
+
+signupButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const fullname = signupForm.fullname.value;
+  const email = signupForm.email.value;
+  const username = signupForm.username.value;
+  const password = signupForm.password.value;
+
+  if (fullname === "" || email === "" || username === "" || password === "") {
+    signupErrorMsg.innerHTML = "Please fill out all fields.";
+    signupErrorMsg.style.opacity = 1;
+  } else {
+    alert("Thank you for signing up! You can now login.");
+    window.location.href = "login.html";
+  }
+});
+
+
+const liTags = document.querySelectorAll('li');
+
+liTags.forEach(li => {
+  li.addEventListener('click', () => {
+    li.classList.toggle('clicked');
+  });
+});
